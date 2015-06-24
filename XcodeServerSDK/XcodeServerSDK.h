d3638a05ddf77f0aaaef53093f8c1a0427faec0b
esttorhe <me@estebantorr.es>
2015-06-23 20:19:47 -0600

    Restored missing file.

---
+++ XcodeServerSDK/XcodeServerSDK.h
@@ -0,0 +1,21 @@
+//
+//  XcodeServerSDK.h
+//  XcodeServerSDK
+//
+//  Created by Honza Dvorsky on 11/06/2015.
+//  Copyright (c) 2015 Honza Dvorsky. All rights reserved.
+//
+
+#import "TargetConditionals.h"
+
+@import Foundation;
+
+//! Project version number for XcodeServerSDK.
+FOUNDATION_EXPORT double XcodeServerSDKVersionNumber;
+
+//! Project version string for XcodeServerSDK.
+FOUNDATION_EXPORT const unsigned char XcodeServerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XcodeServerSDK/PublicHeader.h>
+
+
