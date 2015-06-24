0a9310d001c9372713ba7da052e55388a3af45fc
Mateusz Zając <zajac.mateusz@icloud.com>
2015-06-24 15:54:24 +0200

    Create HTTPUtils test class

---
+++ XcodeServerSDKTests/HTTPUtilsTests.swift
@@ -0,0 +1,29 @@
+//
+//  HTTPUtilsTests.swift
+//  XcodeServerSDK
+//
+//  Created by Mateusz Zając on 24.06.2015.
+//  Copyright © 2015 Honza Dvorsky. All rights reserved.
+//
+
+import XCTest
+import XcodeServerSDK
+
+class HTTPUtilsTests: XCTestCase {
+
+    // MARK: HTTP extension tests
+    func testStringForQuery() {
+        let query = [
+            "Key1": "Value1",
+            "Key2": "Value2",
+            "Key3": "Value3"
+        ]
+        
+        XCTAssertEqual(HTTP.stringForQuery(query), "?Key3=Value3&Key1=Value1&Key2=Value2")
+    }
+    
+    func testOptionalStringForQuery() {
+        XCTAssertEqual(HTTP.stringForQuery(nil), "")
+    }
+
+}
