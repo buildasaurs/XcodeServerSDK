6a917757da837587ad9aaf063118a03febb1afa8
Mateusz Zając <zajac.mateusz@icloud.com>
2015-06-24 16:43:05 +0200

    Swift 2 changes in implementation and header docs

--- XcodeServerSDK/XcodeServerEndpoints.swift
+++ XcodeServerSDK/XcodeServerEndpoints.swift
@@ -22,8 +22,14 @@ public class XcodeServerEndPoints {
     
     let serverConfig: XcodeServerConfig
     
+    /**
+    Designated initializer.
+    
+    - parameter serverConfig: Object of XcodeServerConfig class
+    
+    - returns: Initialized object of XcodeServer endpoints
+    */
     public init(serverConfig: XcodeServerConfig) {
-        
         self.serverConfig = serverConfig
     }
     
@@ -33,101 +39,112 @@ public class XcodeServerEndPoints {
         
         switch endpoint {
             
-        case .Bots:
-            
-            let bots = "\(base)/bots"
-            if let bot = params?["bot"] {
-                let bot = "\(bots)/\(bot)"
-                if let rev = params?["rev"] {
-                    let rev = "\(bot)/\(rev)"
-                    return rev
-                }
-                return bot
-            }
-            return bots
-            
-        case .Integrations:
-            
-            if let _ = params?["bot"] {
-                //gets a list of integrations for this bot
-                let bots = self.endpointURL(.Bots, params: params)
-                return "\(bots)/integrations"
-            }
-            
-            let integrations = "\(base)/integrations"
-            if let integration = params?["integration"] {
+            case .Bots:
                 
-                let oneIntegration = "\(integrations)/\(integration)"
-                return oneIntegration
-            }
-            return integrations
-            
-        case .CancelIntegration:
-            
-            let integration = self.endpointURL(.Integrations, params: params)
-            let cancel = "\(integration)/cancel"
-            return cancel
-            
-        case .Devices:
-            
-            let devices = "\(base)/devices"
-            return devices
-            
-        case .UserCanCreateBots:
-            
-            let users = "\(base)/auth/isBotCreator"
-            return users
-            
-        case .Login:
-            
-            let login = "\(base)/auth/login"
-            return login
-            
-        case .Logout:
-            
-            let logout = "\(base)/auth/logout"
-            return logout
-            
+                let bots = "\(base)/bots"
+                if let bot = params?["bot"] {
+                    let bot = "\(bots)/\(bot)"
+                    if let rev = params?["rev"] {
+                        let rev = "\(bot)/\(rev)"
+                        return rev
+                    }
+                    return bot
+                }
+                return bots
+                
+            case .Integrations:
+                
+                if let _ = params?["bot"] {
+                    //gets a list of integrations for this bot
+                    let bots = self.endpointURL(.Bots, params: params)
+                    return "\(bots)/integrations"
+                }
+                
+                let integrations = "\(base)/integrations"
+                if let integration = params?["integration"] {
+                    
+                    let oneIntegration = "\(integrations)/\(integration)"
+                    return oneIntegration
+                }
+                return integrations
+                
+            case .CancelIntegration:
+                
+                let integration = self.endpointURL(.Integrations, params: params)
+                let cancel = "\(integration)/cancel"
+                return cancel
+                
+            case .Devices:
+                
+                let devices = "\(base)/devices"
+                return devices
+                
+            case .UserCanCreateBots:
+                
+                let users = "\(base)/auth/isBotCreator"
+                return users
+                
+            case .Login:
+                
+                let login = "\(base)/auth/login"
+                return login
+                
+            case .Logout:
+                
+                let logout = "\(base)/auth/logout"
+                return logout
+                
         }
     }
     
-    func createRequest(method: HTTP.Method, endpoint: Endpoint, params: [String : String]? = nil, query: [String : String]? = nil, body:NSDictionary? = nil, doBasicAuth: Bool = true) -> NSMutableURLRequest? {
-        
+    /**
+    Builder method for URlrequests based on input parameters.
+    
+    - parameter method:      HTTP method used for request (GET, POST etc.)
+    - parameter endpoint:    Endpoint object
+    - parameter params:      URL params (default is nil)
+    - parameter query:       Query parameters (default is nil)
+    - parameter body:        Request's body (default is nil)
+    - parameter doBasicAuth: Requirement of authorization (default is true)
+    
+    - returns: NSMutableRequest or nil if wrong URL was provided
+    */
+    func createRequest(method: HTTP.Method, endpoint: Endpoint, params: [String : String]? = nil, query: [String : String]? = nil, body: NSDictionary? = nil, doBasicAuth auth: Bool = true) -> NSMutableURLRequest? {
         let endpointURL = self.endpointURL(endpoint, params: params)
         let queryString = HTTP.stringForQuery(query)
         let wholePath = "\(self.serverConfig.host):\(self.serverConfig.port)\(endpointURL)\(queryString)"
         
-        if let url = NSURL(string: wholePath) {
-            
-            let request = NSMutableURLRequest(URL: url)
-            
-            request.HTTPMethod = method.rawValue
-            
-            if doBasicAuth {
-                //add authorization header
-                let user = self.serverConfig.user ?? ""
-                let password = self.serverConfig.password ?? ""
-                let plainString = "\(user):\(password)" as NSString
-                let plainData = plainString.dataUsingEncoding(NSUTF8StringEncoding)
-                let base64String = plainData?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
-                request.setValue("Basic \(base64String!)", forHTTPHeaderField: "Authorization")
-            }
-            
-            if let body = body {
-                do {
-                    let data = try NSJSONSerialization.dataWithJSONObject(body, options: .PrettyPrinted)
-                    request.HTTPBody = data
-                    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-                } catch {
-                    let error = error as NSError
-                    Log.error("Parsing error \(error.description)")
-                    return nil
-                }
-            }
-            
-            return request
+        guard let url = NSURL(string: wholePath) else {
+            return nil
         }
-        return nil
+        
+        let request = NSMutableURLRequest(URL: url)
+        
+        request.HTTPMethod = method.rawValue
+        
+        if auth {
+            //add authorization header
+            let user = self.serverConfig.user ?? ""
+            let password = self.serverConfig.password ?? ""
+            let plainString = "\(user):\(password)" as NSString
+            let plainData = plainString.dataUsingEncoding(NSUTF8StringEncoding)
+            let base64String = plainData?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+            request.setValue("Basic \(base64String!)", forHTTPHeaderField: "Authorization")
+        }
+        
+        if let body = body {
+            do {
+                let data = try NSJSONSerialization.dataWithJSONObject(body, options: .PrettyPrinted)
+                request.HTTPBody = data
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            } catch {
+                let error = error as NSError
+                Log.error("Parsing error \(error.description)")
+                return nil
+            }
+        }
+        
+        return request
     }
     
 }
