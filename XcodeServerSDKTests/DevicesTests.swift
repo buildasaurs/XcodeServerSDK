e05cfa850b03dd330110f4eccbec24c39db1324c
Mateusz Zajac <cojoj@icloud.com>
2015-07-13 13:07:15 +0200

    Add another assertion

--- XcodeServerSDKTests/DevicesTests.swift
+++ XcodeServerSDKTests/DevicesTests.swift
@@ -11,34 +11,33 @@ import XcodeServerSDK
 
 class DevicesTests: XCTestCase {
     
+    let macMini = Device(json: [
+        "osVersion" : "10.11",
+        "connected" : true,
+        "simulator" : false,
+        "modelCode" : "Macmini6,1",
+        "deviceType" : "com.apple.mac",
+        "modelName" : "Mac mini",
+        "_id" : "1ad0e8785cacca73d980cdb23600383e",
+        "modelUTI" : "com.apple.macmini-unibody-no-optical",
+        "doc_type" : "device",
+        "trusted" : true,
+        "name" : "bozenka",
+        "supported" : true,
+        "processor" : "2,5 GHz Intel Core i5",
+        "identifier" : "A89DE14F-0F3C-5893-AF8A-D9CFF068B82C-x86_64",
+        "enabledForDevelopment" : true,
+        "serialNumber" : "C07JT7H5DWYL",
+        "platformIdentifier" : "com.apple.platform.macosx",
+        "_rev" : "3-6a49ba8135f3f4ddcbaefe1b469f479c",
+        "architecture" : "x86_64",
+        "retina" : false,
+        "isServer" : true,
+        "tinyID" : "7DCD98A"
+        ])
+    
     func testDictionarify() {
-        let json = [
-            "osVersion" : "10.11",
-            "connected" : true,
-            "simulator" : false,
-            "modelCode" : "Macmini6,1",
-            "deviceType" : "com.apple.mac",
-            "modelName" : "Mac mini",
-            "_id" : "1ad0e8785cacca73d980cdb23600383e",
-            "modelUTI" : "com.apple.macmini-unibody-no-optical",
-            "doc_type" : "device",
-            "trusted" : true,
-            "name" : "bozenka",
-            "supported" : true,
-            "processor" : "2,5 GHz Intel Core i5",
-            "identifier" : "A89DE14F-0F3C-5893-AF8A-D9CFF068B82C-x86_64",
-            "enabledForDevelopment" : true,
-            "serialNumber" : "C07JT7H5DWYL",
-            "platformIdentifier" : "com.apple.platform.macosx",
-            "_rev" : "3-6a49ba8135f3f4ddcbaefe1b469f479c",
-            "architecture" : "x86_64",
-            "retina" : false,
-            "isServer" : true,
-            "tinyID" : "7DCD98A"
-        ]
-        let macMini = Device(json: json)
         let expected = [ "device_id": "1ad0e8785cacca73d980cdb23600383e" ]
-        
         XCTAssertEqual(macMini.dictionarify(), expected)
     }
     
@@ -52,6 +51,7 @@ class DevicesTests: XCTestCase {
             
             if let devices = devices {
                 XCTAssertEqual(devices.count, 13, "There should be 13 devices")
+                XCTAssertTrue(devices.map { $0.identifier }.contains(self.macMini.identifier))
             }
             
             expectation.fulfill()
