02ddc5692a425086eb63f5046f6aea6c67024918
Paweł Kowalczyk <pmkowal@gmail.com>
2015-07-24 11:03:33 +0200

    XcodeServerEndpointsTests - endpointURL(.Bots) tests

--- XcodeServerSDKTests/XcodeServerEndpointsTests.swift
+++ XcodeServerSDKTests/XcodeServerEndpointsTests.swift
@@ -25,5 +25,61 @@ class XcodeServerEndpointsTests: XCTestCase {
         let expectation = endpoints?.createRequest(.GET, endpoint: .Bots, params: ["test": "test"], query: ["test//http\\": "!test"], body: ["test": "test"], doBasicAuth: true)
         XCTAssertNil(expectation, "Shouldn't create request from malformed URL")
     }
-
+    
+    // MARK: endpointURL(.Bots)
+    
+    func testEndpointURLCreationForBotsWithoutParams() {
+        let url = self.endpoints?.endpointURL(.Bots)
+        XCTAssertEqual(url!, "/api/bots", "endpointURL(.Bots) should return \"/api/bots\"")
+    }
+    
+    func testEndpointURLCreationForBots() {
+        let paramsArray = [
+            [
+                "key": "value"
+            ],
+            [
+                "key": "value",
+                "rev": "rev"
+            ]
+        ]
+        for params in paramsArray {
+            let url = self.endpoints?.endpointURL(.Bots, params: params)
+            XCTAssertEqual(url!, "/api/bots", "endpointURL(.Bots, \(params)) should return \"/api/bots\"")
+        }
+    }
+    
+    func testEndpointURLCreationForBotsBot() {
+        let paramsArray = [
+            [
+                "bot": "botValue"
+            ],
+            [
+                "otherKey": "otherValue",
+                "bot": "botValue"
+            ]
+        ]
+        for params in paramsArray {
+            let url = self.endpoints?.endpointURL(.Bots, params: params)
+            XCTAssertEqual(url!, "/api/bots/botValue", "endpointURL(.Bots, \(params)) should return \"/api/bots/botValue\"")
+        }
+    }
+    
+    func testEndpointURLCreationForBotsBotRev() {
+        let paramsArray = [
+            [
+                "rev": "revValue",
+                "bot": "botValue"
+            ],
+            [
+                "otherKey": "otherValue",
+                "bot": "botValue",
+                "rev": "revValue"
+            ]
+        ]
+        for params in paramsArray {
+            let url = self.endpoints?.endpointURL(.Bots, params: params)
+            XCTAssertEqual(url!, "/api/bots/botValue/revValue", "endpointURL(.Bots, \(params)) should return \"/api/bots/botValue/revValue\"")
+        }
+    }
 }
