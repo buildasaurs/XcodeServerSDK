2cde22d668b7f81f127a0c535d4645e881647bdb
Mateusz Zajac <cojoj@icloud.com>
2015-07-24 11:36:44 +0200

    Minor changes (still need some polish)

--- XcodeServerSDK/Server Entities/IntegrationCommits.swift
+++ XcodeServerSDK/Server Entities/IntegrationCommits.swift
@@ -13,17 +13,30 @@ public class IntegrationCommits: XcodeServerEntity {
     public let integration: String
     public let botTinyID: String
     public let botID: String
-    public let commits: [Commit]
+    public let commits: [String: [Commit]]
     public let endedTimeDate: NSDate
     
     public required init(json: NSDictionary) {
         self.integration = json.stringForKey("integration")
         self.botTinyID = json.stringForKey("botTinyID")
         self.botID = json.stringForKey("botID")
-        self.commits = json.dictionaryForKey("commits").allValues.flatMap { Commit(json: $0 as! NSDictionary) }
+        self.commits = IntegrationCommits.populateCommits(json.dictionaryForKey("commits"))
         self.endedTimeDate = NSDate()
         
         super.init(json: json)
+    }
+    
+    class func populateCommits(json: NSDictionary) -> [String: [Commit]] {
+        var resultsDictionary: [String: [Commit]] = Dictionary()
+        
+        for (key, value) in json {
+            let blueprintID = key as! String
+            let commitsArray = (value as! NSArray) as Array
+            
+            resultsDictionary[blueprintID] = commitsArray.map { Commit(json: $0 as! NSDictionary) }
+        }
+        
+        return resultsDictionary
     }
     
 }
