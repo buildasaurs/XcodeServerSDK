b42f8bf1be0e7621901d10d758ce73cafc685673
Mateusz Zajac <cojoj@icloud.com>
2015-07-27 09:18:56 +0200

    Write more assertions

--- XcodeServerSDKTests/IntegrationTests.swift
+++ XcodeServerSDKTests/IntegrationTests.swift
@@ -23,13 +23,39 @@ class IntegrationTests: XCTestCase {
         self.waitForExpectationsWithTimeout(10, handler: nil)
     }
     
-    // MAKR: Commits
+    // MARK: Commits
+    
+    var expectedDate: NSDate = {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ"
+        
+        return formatter.dateFromString("2015-07-24T09:40:58.462Z")!
+    }()
+    
     func testGetIntegrationCommits() {
         
         let exp = self.expectationWithDescription("Network")
         let server = self.getRecordingXcodeServer("get_integration_commits")
-        server.getIntegrationCommits("56ad016e2e3993ca0b8ed276050150e8") { (commitIntegrations, error) in
+        server.getIntegrationCommits("56ad016e2e3993ca0b8ed276050150e8") { (integrationCommits, error) in
             XCTAssertNil(error, "Error should be nil")
+            
+            guard let integrationCommits = integrationCommits else {
+                XCTFail("Integration commits are empty")
+                return
+            }
+            
+            // Casette assertions
+            XCTAssertEqual(integrationCommits.endedTimeDate, self.expectedDate)
+            
+            guard let commits = integrationCommits.commits["A36AEFA3F9FF1F738E92F0C497C14977DCE02B97"] else {
+                XCTFail("Commits dictionary is empty but should have one key")
+                return
+            }
+            
+            XCTAssertEqual(commits.count, 6)
+            
+            let commiters = Set(commits.map { $0.contributor.name })
+            XCTAssertEqual(commiters.count, 2)
             
             exp.fulfill()
         }
