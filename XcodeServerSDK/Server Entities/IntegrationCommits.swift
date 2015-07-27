d74b88c2ccc712e976024b6ac52535503340418f
Mateusz Zajac <cojoj@icloud.com>
2015-07-27 09:53:10 +0200

    Move to function

--- XcodeServerSDK/Server Entities/IntegrationCommits.swift
+++ XcodeServerSDK/Server Entities/IntegrationCommits.swift
@@ -22,27 +22,7 @@ public class IntegrationCommits: XcodeServerEntity {
         self.botTinyID = json.stringForKey("botTinyID")
         self.botID = json.stringForKey("botID")
         self.commits = IntegrationCommits.populateCommits(json.dictionaryForKey("commits"))
-        self.endedTimeDate = {
-            let dateArray = json.arrayForKey("endedTimeDate") as [Int]
-            do {
-                let stringDate = try dateArray.dateString()
-                let formatter = NSDateFormatter()
-                formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ"
-                
-                guard let date = formatter.dateFromString(stringDate) else {
-                    Log.error("Formatter couldn't parse date")
-                    return NSDate()
-                }
-                
-                return date
-            } catch DateParsing.WrongNumberOfElements(let elements) {
-                Log.error("Couldn't parse date as Array has \(elements) elements")
-            } catch {
-                Log.error("Something went wrong while parsing date")
-            }
-            
-            return NSDate()
-        }()
+        self.endedTimeDate = IntegrationCommits.parseDate(json.arrayForKey("endedTimeDate"))
         
         super.init(json: json)
     }
@@ -60,6 +40,29 @@ public class IntegrationCommits: XcodeServerEntity {
         }
         
         return resultsDictionary
+    }
+    
+    class func parseDate(array: NSArray) -> NSDate {
+        let dateArray = array as! [Int]
+        
+        do {
+            let stringDate = try dateArray.dateString()
+            let formatter = NSDateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ"
+            
+            guard let date = formatter.dateFromString(stringDate) else {
+                Log.error("Formatter couldn't parse date")
+                return NSDate()
+            }
+            
+            return date
+        } catch DateParsing.WrongNumberOfElements(let elements) {
+            Log.error("Couldn't parse date as Array has \(elements) elements")
+        } catch {
+            Log.error("Something went wrong while parsing date")
+        }
+        
+        return NSDate()
     }
     
 }
