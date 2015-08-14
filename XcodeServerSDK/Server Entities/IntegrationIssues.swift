b3ce3d69e4fdc9cd595d42bee7695e4a4919ee2d
Mateusz Zajac <cojoj@icloud.com>
2015-08-14 10:23:11 +0200

    Create IntegrationIssues class

---
+++ XcodeServerSDK/Server Entities/IntegrationIssues.swift
@@ -0,0 +1,38 @@
+//
+//  IntegrationIssues.swift
+//  XcodeServerSDK
+//
+//  Created by Mateusz Zając on 12.08.2015.
+//  Copyright © 2015 Honza Dvorsky. All rights reserved.
+//
+
+import Foundation
+import BuildaUtils
+
+public class IntegrationsIssue: XcodeServerEntity {
+    
+    public let buildServiceErrors: [Issue]
+    public let buildServiceWarnings: [Issue]
+    public let triggerErrors: [Issue]
+    public let errors: [Issue]
+    public let warnings: [Issue]
+    public let testFailures: [Issue]
+    public let analyzerWarnings: [Issue]
+    
+    // MARK: Initialization
+    
+    public required init(json: NSDictionary) {
+        self.buildServiceErrors = json.arrayForKey("buildServiceErrors").map { Issue(json: $0) }
+        self.buildServiceWarnings = json.arrayForKey("buildServiceWarnings").map { Issue(json: $0) }
+        self.triggerErrors = json.arrayForKey("triggerErrors").map { Issue(json: $0) }
+        
+        // Nested issues
+        self.errors = json.dictionaryForKey("errors").allValues.map { Issue(json: $0 as! NSDictionary) }
+        self.warnings = json.dictionaryForKey("warnings").allValues.map { Issue(json: $0 as! NSDictionary) }
+        self.testFailures = json.dictionaryForKey("testFailures").allValues.map { Issue(json: $0 as! NSDictionary) }
+        self.analyzerWarnings = json.dictionaryForKey("analyzerWarnings").allValues.map { Issue(json: $0 as! NSDictionary) }
+        
+        super.init(json: json)
+    }
+    
+}
